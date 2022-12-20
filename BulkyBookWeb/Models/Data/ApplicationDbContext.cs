a6907d7058ebf6cd74ae176f8c303fc28e0de562
MuhittinBasarErsoy <basarersoy@gmail.com>
2022-12-20 21:02:15 +0300

    Add project files.

---
+++ BulkyBookWeb/Models/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Models.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //add-migration AddCategoryToDatabase
+        //update-database
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; } 
+    }
+}
