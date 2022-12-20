3532a0787f10d24848e42027fa3b9a985822fe79
MuhittinBasarErsoy <basarersoy@gmail.com>
2022-12-20 21:20:04 +0300

    Get method for Edit

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -26,11 +26,44 @@ namespace BulkyBookWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            if(obj.Name == obj.DisplayOrder.ToString())
+            if (obj.Name == obj.DisplayOrder.ToString())
             {
-                ModelState.AddModelError("CustomError","The DisplayOrder cannot exactly match the Name.");
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the Name.");
             }
-            if(ModelState.IsValid) {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            var categoryFromDbDbFirst = _db.Categories.FirstOrDefault(categoryFromDb => categoryFromDb.Id == id);
+            var categoryFromDbDbSingle = _db.Categories.SingleOrDefault(categoryFromDb => categoryFromDb.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
