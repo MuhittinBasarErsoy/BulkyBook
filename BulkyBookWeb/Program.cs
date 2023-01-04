4fa4b57b2f2ffd87d12617b79974c526621479f9
MuhittinBasarErsoy <basarersoy@gmail.com>
2023-01-04 16:43:11 +0300

    Create Roles for Website

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -4,6 +4,8 @@ using BulkyBook.DataAccess.Repository;
 using BulkyBook.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,9 +15,11 @@ builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServe
      builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 
-builder.Services.AddDefaultIdentity<IdentityUser>()
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddSingleton<IEmailSender, EmailSender>();
+builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
