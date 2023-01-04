f8a4cc90511e372b913754982c50b600d36fbd5e
MuhittinBasarErsoy <basarersoy@gmail.com>
2023-01-04 14:14:49 +0300

    Login and register menus added as partial

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServe
      builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 var app = builder.Build();
