6c753e280a081e09e7229f3625e22113b4132e8d
MuhittinBasarErsoy <basarersoy@gmail.com>
2023-01-04 15:36:30 +0300

    Company CRUD

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -4,18 +4,19 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BulkyBook.DataAccess.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
-    {
-        //add-migration AddCategoryToDatabase
-        //update-database
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		//add-migration AddCategoryToDatabase
+		//update-database
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
 
-        }
+		}
 
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<CoverType> CoverTypes { get; set; }
-        public DbSet<Product> Products { get; set; }
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<CoverType> CoverTypes { get; set; }
+		public DbSet<Product> Products { get; set; }
 		public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+		public DbSet<Company> Companies { get; set; }
 	}
 }
--- BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -17,6 +17,7 @@ namespace BulkyBook.DataAccess.Repository
             Category = new CategoryRepository(_db);
             CoverType = new CoverTypeRepository(_db);
             Product = new ProductRepository(_db);
+            Company = new CompanyRepository(_db);
 
         }
 
@@ -24,8 +25,9 @@ namespace BulkyBook.DataAccess.Repository
         public ICategoryRepository Category { get; private set; }
         public ICoverTypeRepository CoverType { get; private set; }
         public IProductRepository Product { get; private set; }
+		public ICompanyRepository Company { get; private set; }
 
-        public void Save()
+		public void Save()
         {
             _db.SaveChanges();
         }
